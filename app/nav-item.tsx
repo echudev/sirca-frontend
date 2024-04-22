de23565449b94d2f5e3f6890a65c91e839504429
Ezequiel <ezequiel.maranda@gmail.com>
2024-04-22 12:32:58 -0300

    accordion sidebar menu

--- app/nav-item.tsx
+++ app/nav-item.tsx
@@ -6,10 +6,12 @@ import { usePathname } from 'next/navigation';
 
 export function NavItem({
   size = 1,
+  toggleState,
   href,
   children
 }: {
   size?: number;
+  toggleState?: () => void | undefined;
   href: string;
   children: React.ReactNode;
 }) {
@@ -18,6 +20,7 @@ export function NavItem({
   return (
     <Link
       href={href}
+      onClick={toggleState}
       className={clsx(
         'animate-in slide-in-from-left flex items-center gap-2 rounded-lg px-3 py-2 text-base text-gray-900 transition-all hover:bg-gray-200/90  dark:text-gray-50 dark:hover:text-gray-50',
         {
--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { clsx } from 'clsx';
 import { NavItem } from './nav-item';
 import {
   ScreenIcon,
@@ -7,6 +11,7 @@ import {
 } from '../components/icons';
 
 export default function Sidebar() {
+  const pathname = usePathname();
   return (
     <div className="flex-1 overflow-auto py-2">
       <nav className="flex flex-col tems-start px-4 text-sm font-medium h-full">
@@ -18,34 +23,51 @@ export default function Sidebar() {
           <HammerIcon />
           Mantenimiento
         </NavItem>
-        <NavItem size={2} href="/mantenimiento/reparaciones">
-          Equipos
-        </NavItem>
-        <NavItem size={2} href="/mantenimiento/reparaciones">
-          Calibraciones
-        </NavItem>
-        <NavItem size={2} href="/mantenimiento/reparaciones">
-          Edilicio
-        </NavItem>
-        <NavItem size={2} href="/mantenimiento/reparaciones">
-          Programado
-        </NavItem>
+        <div
+          aria-label="Mantenimiento-content"
+          aria-expanded="false"
+          aria-controls="Mantenimiento-content"
+          className={clsx('h-0 overflow-hidden transition-all', {
+            'h-28': pathname.startsWith('/mantenimiento')
+          })}
+        >
+          <NavItem size={2} href="/mantenimiento/reparaciones">
+            Equipos
+          </NavItem>
+          <NavItem size={2} href="/mantenimiento/reparaciones">
+            Calibraciones
+          </NavItem>
+          <NavItem size={2} href="/mantenimiento/reparaciones">
+            Programado
+          </NavItem>
+        </div>
         <NavItem href="/inventario">
           <ArchiveIcon />
           Inventario
         </NavItem>
-        <NavItem size={2} href="/mantenimiento/reparaciones">
-          Equipos
-        </NavItem>
-        <NavItem size={2} href="/mantenimiento/reparaciones">
-          Repuestos
-        </NavItem>
-        <NavItem size={2} href="/mantenimiento/reparaciones">
-          Gases
-        </NavItem>
-        <NavItem size={2} href="/mantenimiento/reparaciones">
-          Consumibles
-        </NavItem>
+        <div
+          aria-label="Inventario-content"
+          aria-controls="Inventario-content"
+          className={clsx(
+            'h-0 overflow-hidden transition-all animate-in slide-in-from-top',
+            {
+              'h-28': pathname.startsWith('/inventario')
+            }
+          )}
+        >
+          <NavItem size={2} href="/mantenimiento/reparaciones">
+            Equipos
+          </NavItem>
+          <NavItem size={2} href="/mantenimiento/reparaciones">
+            Partes
+          </NavItem>
+          <NavItem size={2} href="/mantenimiento/reparaciones">
+            Gases
+          </NavItem>
+          <NavItem size={2} href="/mantenimiento/reparaciones">
+            Consumibles
+          </NavItem>
+        </div>
         <NavItem href="/calendario">
           <CalendarIcon />
           Calendario
