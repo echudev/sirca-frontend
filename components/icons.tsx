24aee934af25eb2cdc1f64f1ffeb69940443ef16
Ezequiel <ezequiel.maranda@gmail.com>
2024-04-28 19:51:14 -0300

    historial filters & search created

--- components/icons.tsx
+++ components/icons.tsx
@@ -183,3 +183,23 @@ export function ArrowIcon(props: React.SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function SearchIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+      />
+    </svg>
+  );
+}
