fd778adad3c4717e4e5d9e6f7e93dc433aa42d9d
Ezequiel <ezequiel.maranda@gmail.com>
2024-05-28 15:35:37 -0300

    header component abstracted

--- app/mantenimiento/calibraciones/page.tsx
+++ app/mantenimiento/calibraciones/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from 'next';
-import { Header } from './header';
+import { Header } from '../../../components/header';
 
 export const metadata: Metadata = {
   title: 'SIRCA - Mantenimiento',
--- app/mantenimiento/ordenes/page.tsx
+++ app/mantenimiento/ordenes/page.tsx
@@ -2,7 +2,7 @@ import { Filters } from './filters';
 import { Table } from './table';
 import { Metadata } from 'next';
 import GetOrdenes from '../../../lib/getOrdenes';
-import { Header } from './header';
+import { Header } from '../../../components/header';
 
 export const metadata: Metadata = {
   title: 'SIRCA - Mantenimiento',
