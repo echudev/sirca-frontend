de6f4d469822efb7f055394e3e9c2444708027fc
Ezequiel <ezequiel.maranda@gmail.com>
2024-11-28 13:01:55 -0300

    feat: creo la welcome page con layout

---
+++ app/welcome/page.tsx
@@ -0,0 +1,10 @@
+export default async function WelcomePage() {
+  return (
+    <main className="flex flex-1 flex-col p-4 md:p-6">
+      <div className="flex items-center mb-8">
+        <h1 className="font-semibold text-lg md:text-2xl">Panel General</h1>
+      </div>
+      <p>Welcome</p>
+    </main>
+  );
+}
