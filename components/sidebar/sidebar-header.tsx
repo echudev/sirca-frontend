53361a32310cc2dc1545ec306752d4ae9bd9f1a8
Ezequiel <ezequiel.maranda@gmail.com>
2024-12-07 15:49:48 -0300

    style: logo & title color white -> secondary

--- components/sidebar/sidebar-header.tsx
+++ components/sidebar/sidebar-header.tsx
@@ -5,6 +5,7 @@ import {
   SidebarMenuButton,
   useSidebar,
 } from "@/components/ui/sidebar";
+import balogo from "../../public/BA-secondary.png";
 import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
 
 export function SidebarHeaderComponent() {
@@ -16,12 +17,12 @@ export function SidebarHeaderComponent() {
         size="lg"
         className="hover:bg-sidebar active:bg-sidebar hover:text-white active:text-white"
       >
-        <Avatar className={`invert ${open ? "h-14 w-14 " : "h-8 w-8"}`}>
-          <AvatarImage src="https://general.premioseikon.com/wp-content/uploads/2022/09/logo-BA-png-1024x921.png" />
+        <Avatar className={open ? "h-14 w-14 " : "h-8 w-8"}>
+          <AvatarImage src={balogo.src} alt="logo" />
           <AvatarFallback>logo</AvatarFallback>
         </Avatar>
         <div
-          className={`flex flex-col ml-2 transition-transform ${open ? "delay-300 translate-y-0 opacity-100" : "opacity-0 -translate-y-40"}`}
+          className={`flex flex-col ml-2 text-secondary transition-transform ${open ? "delay-300 translate-y-0 opacity-100" : "opacity-0 -translate-y-40"}`}
         >
           <p className="font-bold text-sm">APrA</p>
           <p className="text-xs">Red de Calidad del Aire</p>
