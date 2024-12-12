8488e2ad57f804d1b6bf2a41b4c5ae78d6bee0d8
Ezequiel <ezequiel.maranda@gmail.com>
2024-12-12 02:21:39 -0300

    sidebar styles updated

--- components/sidebar/sidebar-header.tsx
+++ components/sidebar/sidebar-header.tsx
@@ -5,8 +5,9 @@ import {
   SidebarMenuButton,
   useSidebar,
 } from "@/components/ui/sidebar";
-import balogo from "../../public/BA-secondary.png";
+import balogo from "../../public/BA-primary.png";
 import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
+import { cn } from "@/lib/utils";
 
 export function Header() {
   const { open } = useSidebar();
@@ -17,12 +18,16 @@ export function Header() {
         size="lg"
         className="hover:bg-sidebar active:bg-sidebar hover:text-white active:text-white"
       >
-        <Avatar className={open ? "h-14 w-14 " : "h-8 w-8"}>
-          <AvatarImage src={balogo.src} alt="logo" />
+        <Avatar className={open ? "h-14 w-14 rounded-md" : "h-8 w-8"}>
+          <AvatarImage
+            src={balogo.src}
+            alt="logo"
+            className={cn("bg-accent")}
+          />
           <AvatarFallback>BA</AvatarFallback>
         </Avatar>
         <div
-          className={`flex flex-col ml-2 text-secondary transition-transform ${open ? "delay-300 translate-y-0 opacity-100" : "opacity-0 -translate-y-40"}`}
+          className={`flex flex-col ml-2 text-primary-foreground transition-transform ${open ? "delay-300 translate-y-0 opacity-100" : "opacity-0 -translate-y-40"}`}
         >
           <p className="font-bold text-sm">APrA</p>
           <p className="text-xs">Red de Calidad del Aire</p>
