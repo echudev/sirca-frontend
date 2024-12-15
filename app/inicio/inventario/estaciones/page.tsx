7ab88a211d6a2e5813c1d1b593b4c92103cf6473
Ezequiel <ezequiel.maranda@gmail.com>
2024-12-15 20:50:15 -0300

    feat(loader): refactorizo page Estaciones para que muestre loader mientras fetchea la data

--- app/inicio/inventario/estaciones/page.tsx
+++ app/inicio/inventario/estaciones/page.tsx
@@ -1,30 +1,14 @@
 import { Metadata } from "next";
-import { columns } from "./columns";
-import { DataTable } from "@/components/data-table/data-table";
-import { taskSchema } from "./schema";
-import fs from "fs/promises";
-import path from "path";
-import { z } from "zod";
+import { Suspense } from "react";
+import TablaEstaciones from "./tabla-estaciones";
+import Loader from "@/app/loading";
 
 export const metadata: Metadata = {
   title: "SIRCA - Inventario",
   description: "App de inventario de la red",
 };
 
-// Simulate a database read for tasks.
-async function getTasks() {
-  const data = await fs.readFile(
-    path.join(process.cwd(), "/app/inicio/inventario/estaciones/tasks.json")
-  );
-
-  const tasks = JSON.parse(data.toString());
-
-  return z.array(taskSchema).parse(tasks);
-}
-
-export default async function Estaciones() {
-  const data = await getTasks();
-
+export default function Estaciones() {
   return (
     <main className="container mx-auto">
       <div className="flex items-center justify-between space-y-2 my-8">
@@ -38,7 +22,9 @@ export default async function Estaciones() {
           </p>
         </div>
       </div>
-      <DataTable columns={columns} data={data} />
+      <Suspense fallback={<Loader />}>
+        <TablaEstaciones />
+      </Suspense>
     </main>
   );
 }
---
+++ app/inicio/inventario/estaciones/tabla-estaciones.tsx
@@ -0,0 +1,23 @@
+import { columns } from "./columns";
+import { DataTable } from "@/components/data-table/data-table";
+import { taskSchema } from "./schema";
+import fs from "fs/promises";
+import path from "path";
+import { z } from "zod";
+
+// Simulate a database read for tasks.
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "/app/inicio/inventario/estaciones/tasks.json")
+  );
+
+  const tasks = JSON.parse(data.toString());
+
+  return z.array(taskSchema).parse(tasks);
+}
+
+export default async function TablaEstaciones() {
+  const data = await getTasks();
+
+  return <DataTable columns={columns} data={data} />;
+}
