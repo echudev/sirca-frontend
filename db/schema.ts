a1df6a5d1f4df58007a330a50b68e1c95b7eec07
Ezequiel <ezequiel.maranda@gmail.com>
2024-12-15 09:42:37 -0300

    chore: creo type user & newUser a partir de inferencias de pgTable Drizzle

--- db/schema.ts
+++ db/schema.ts
@@ -5,6 +5,7 @@ import {
   text,
   timestamp,
 } from "drizzle-orm/pg-core";
+import { InferSelectModel, InferInsertModel } from "drizzle-orm";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -14,3 +15,8 @@ export const usersTable = pgTable("users", {
   role: varchar({ length: 10 }).default("VIEWER").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
+
+// Tipo para representaciones seleccionadas desde la BD (ej. en queries)
+export type User = InferSelectModel<typeof usersTable>;
+// Tipo para inserciones (ej. datos para INSERT)
+export type NewUser = InferInsertModel<typeof usersTable>;
