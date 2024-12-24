664958cb20bbfdad650a6d1ff61a0b3cfd28eac3
Ezequiel <ezequiel.maranda@gmail.com>
2024-12-24 12:07:09 -0300

    refactor(páginas): elimino componente main duplicado en paginas en construcción

--- app/inicio/calendario/page.tsx
+++ app/inicio/calendario/page.tsx
@@ -7,12 +7,10 @@ export const metadata: Metadata = {
 
 export default async function Configuracion() {
   return (
-    <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
-      <div className="flex items-center">
-        <h1 className="font-semibold text-lg text-primary md:text-2xl">
-          Página en construcción
-        </h1>
-      </div>
-    </main>
+    <div className="flex items-center justify-center h-full">
+      <h1 className="font-semibold text-lg text-primary md:text-2xl">
+        Página en construcción
+      </h1>
+    </div>
   );
 }
--- app/inicio/page.tsx
+++ app/inicio/page.tsx
@@ -1,11 +1,9 @@
 export default function Inicio() {
   return (
-    <main className="flex flex-1 flex-col p-4 md:p-6">
-      <div className="flex items-center">
-        <h1 className="font-semibold text-lg text-primary md:text-2xl">
-          Página en construcción
-        </h1>
-      </div>
-    </main>
+    <div className="flex items-center justify-center h-full">
+      <h1 className="font-semibold text-lg text-primary md:text-2xl">
+        Página en construcción
+      </h1>
+    </div>
   );
 }
