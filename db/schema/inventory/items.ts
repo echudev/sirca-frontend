504973680ccebafd0a93e3fa1b6d876825478d70
Ezequiel <ezequiel.maranda@gmail.com>
2024-12-29 09:56:29 -0300

    refactor(db): agrego brand a tabla items para poder agregar en join a repuestos que tengan solo marca sin modelo

--- db/schema/inventory/items.ts
+++ db/schema/inventory/items.ts
@@ -14,12 +14,16 @@ import { sparePartsDetail } from "./spare-part-detail";
 import { sparePartAnalyzer } from "./spare-part-analyzer";
 import { inventory } from "./inventory";
 import { traslados } from "./traslados";
+import { brands } from "./brands";
 import { models } from "./models";
 import { commonColumns } from "../common-columns";
 
 export const items = pgTable("items", {
   id: serial("item_id").primaryKey(),
   itemType: varchar("item_type", { length: 20 }).notNull().$type<ItemType>(),
+  brands: integer("item_brand_id")
+    .notNull()
+    .references(() => brands.id, { onDelete: "cascade" }),
   models: integer("item_model_id")
     .notNull()
     .references(() => models.id, { onDelete: "cascade" }),
@@ -35,6 +39,7 @@ export const items = pgTable("items", {
 export const itemsRelations = relations(items, ({ one, many }) => ({
   analyzer: one(analyzersDetail),
   cylinder: one(cylindersDetail),
+  brand: one(brands),
   model: one(models),
   spareParts: one(sparePartsDetail),
   inventory: many(inventory),
