e8c31700e68563d82c00dc76e7336fd71255ac5f
Ezequiel <ezequiel.maranda@gmail.com>
2025-01-10 11:28:34 -0300

    feat(partes): agregar revalidación de 60 segundos para el listado de partes

--- app/inicio/inventario/partes/page.tsx
+++ app/inicio/inventario/partes/page.tsx
@@ -12,6 +12,7 @@ export const metadata: Metadata = {
 };
 
 const title = "Listado de Partes";
+export const revalidate = 60
 
 export default async function Partes() {
   const data = await handleGetPartes();
